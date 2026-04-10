1e53dbfe94756e9fff36889e692fb053421b4827
mabar <mabar1984@gmail.com>
2026-04-10 19:05:16 +0300

    04\02.js is done

--- lesson-04/02.js
+++ lesson-04/02.js
@@ -15,4 +15,12 @@
 Подсказка: можно использовать функцию `includesElement`, которую мы написали ранее. Переписывать её не нужно, она доступна по всему проекту за счёт hoisting.
 */
 
-function findUniqueElements() {}
+function findUniqueElements(arr) {
+    const res = []
+    for (let i = 0; i < arr.length; i++) {
+        if (!includesElement(res, arr[i])) {
+            res.push(arr[i])
+        }
+    }
+    return res
+}
