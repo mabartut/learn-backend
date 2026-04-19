d601cb96116a335844d47e04eb9789dcd77b7cbb
mabar <mabar1984@gmail.com>
2026-04-19 22:09:00 +0300

    ex 11 is done

---
+++ precourse/nodejs/exams/exam11.js
@@ -0,0 +1,23 @@
+// Вопрос1:
+//     Верни JSON {"status":"ok"}. Укажи корректный Content-Type.
+//     Подсказка: javascript объект можно преобразовать в JSON с помощью утилиты JSON.stringify
+//
+// // внутри обработчика:
+// res.writeHead(200, { 'Content-Type': XXX });
+// res.end(YYY);
+// Твой ответ:
+//     'application/json' JSON.stringify({"status": "ok"})
+//
+//
+// /////////////////////////
+// Вопрос6:
+//     Верни 405 Method Not Allowed, если метод не GET
+//
+// if (req.method === XXX) {
+//     res.end('ok');
+// } else {
+//     YYY
+//     res.end('Method Not Allowed');
+// }
+// Твой ответ:
+//     'GET' res.writeHead(405, {'Content-Type': 'text/plain'})
