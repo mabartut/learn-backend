5025472d4d646d26df23998521f3fadbe7854db5
mabar <mabar1984@gmail.com>
2026-04-23 20:20:20 +0300

    start timer-db

---
+++ precourse/timer-app/src/db.js
@@ -0,0 +1,6 @@
+import { Pool } from'pg';
+
+const connectionString =
+    process.env.DATABASE_URL || 'postgresql://postgres:password@localhost:5532/timerdb';
+
+export const pool = new Pool({ connectionString });
