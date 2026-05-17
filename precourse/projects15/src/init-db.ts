5f1c4551c18dca2702b4e78524f229dda397dcd3
mabar <mabar1984@gmail.com>
2026-05-17 21:59:29 +0300

    CREATE TABLE tasks added in init-db.ts

--- precourse/projects15/src/init-db.ts
+++ precourse/projects15/src/init-db.ts
@@ -1,18 +1,30 @@
 // src/init-db.ts
-import { pool } from "./db";
+import {pool} from "./db";
 
 async function init() {
     await pool.query(`
-    CREATE TABLE IF NOT EXISTS projects (
-      id SERIAL PRIMARY KEY,
-      name TEXT NOT NULL,
-      description TEXT NOT NULL DEFAULT '',
-      status TEXT NOT NULL DEFAULT 'todo',
-      created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-      CONSTRAINT projects_status_check CHECK (status IN ('todo','in_progress','done'))
-    );
-  `);
+        CREATE TABLE IF NOT EXISTS projects
+        (
+            id          SERIAL PRIMARY KEY,
+            name        TEXT        NOT NULL,
+            description TEXT        NOT NULL DEFAULT '',
+            status      TEXT        NOT NULL DEFAULT 'todo',
+            created_at  TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+            CONSTRAINT projects_status_check CHECK (status IN ('todo', 'in_progress', 'done'))
+        );
+
+        CREATE TABLE IF NOT EXISTS tasks
+        (
+            id         SERIAL PRIMARY KEY,
+            project_id INT                NOT NULL,
+            title      TEXT               NOT NULL,
+            is_done    BOOLEAN            NOT NULL DEFAULT false,
+            created_at TIMESTAMPTZ        NOT NULL DEFAULT NOW(),
+            FOREIGN KEY (project_id) REFERENCES projects (id) ON DELETE CASCADE
+        );
+    `);
     console.log("✅ Таблица projects готова");
     await pool.end();
 }
+
 init();
